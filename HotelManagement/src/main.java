e6a9dcbbeab4faff77f08999c56bc320370f3a33
Mahmoud Kharbat <mahmoodkharbat@gmail.com>
2023-09-01 20:24:35 +0300

    hello yazan

--- HotelManagement/src/main.java
+++ HotelManagement/src/main.java
@@ -8,7 +8,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
-		//fewfwfweefwefewfewf
+		System.out.println("hello yazan");
 	}
 
 }
