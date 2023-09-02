18c8e7ca4a87e793357373e1c8390319bb54914e
YazanIg <yazanigbariya@gmail.com>
2023-09-02 18:42:37 +0300

    1234

--- HotelManagement/src/main.java
+++ HotelManagement/src/main.java
@@ -1,21 +1,56 @@
 import javafx.application.Application;
+import java.awt.EventQueue;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-public class main extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello JavaFX");
-        Label label = new Label("Hello, JavaFX!");
-        StackPane root = new StackPane();
-        root.getChildren().add(label);
-        primaryStage.setScene(new Scene(root, 300, 200));
-        primaryStage.show();
-    }
-
+public class main {
     public static void main(String[] args) {
-        launch(args);
+        // Database connection details
+        String databaseUrl = "jdbc:mysql://localhost:3306/HotelDBConnection";
+        String username = "root";
+        String password = "Yazan.11";
+
+        Connection connection = null;
+
+        try {
+            // Establish a database connection
+            connection = DriverManager.getConnection(databaseUrl, username, password);
+
+            if (connection != null) {
+                System.out.println("Connected to the database!");
+                // You can perform database operations here.
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            // Close the database connection when done
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // Create an instance of the AppWindow class and display the GUI
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    AppWindow window = new AppWindow();
+                    window.getFrame().setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
+
