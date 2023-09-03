4ff4ba4a920c2b45647949ae1b8f541b9ea46ebc
Mahmoud Kharbat <mahmoodkharbat@gmail.com>
2023-09-03 22:24:50 +0300

    add sql connection

--- HotelManagement/src/main_hotel.java
+++ HotelManagement/src/main_hotel.java
@@ -4,29 +4,22 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-<<<<<<< HEAD
-import java.sql.*;
-=======
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
->>>>>>> branch 'main' of https://github.com/MahmoudKharbat/hotel-project.git
 
-public class main {
+import java.sql.*;
+
+public class main_hotel {
     public static void main(String[] args) {
         // Database connection details
-        String databaseUrl = "jdbc:mysql://localhost:3306/HotelDBConnection";
+        String databaseUrl = "jdbc:mysql://localhost:3306/hoteldatabase";
         String username = "root";
-        String password = "Yazan.11";
+        String password = "123456789";
 
         Connection connection = null;
 
         try {
             // Establish a database connection
             connection = DriverManager.getConnection(databaseUrl, username, password);
-
+            System.out.println("hello world");
             if (connection != null) {
                 System.out.println("Connected to the database!");
                 // You can perform database operations here.
